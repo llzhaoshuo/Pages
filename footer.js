8c05cd850825d0135095d1c99db7c5397c2472f3
llzhaoshuo <l_zhaoshuo@163.com>
2023-03-18 11:22:28 +0800

    test

---
+++ footer.js
@@ -0,0 +1,4 @@
+document.writeln("    <div class=\"footer-content\">")
+document.writeln("    	<p><a href=\"http://www.neu.edu.cn\">东北大学 计算机科学与工程学院 人工智能系<a></p>")
+document.writeln("    <p style=\"font-size:14px\">Copyright 2023 DFLAG-NEU. All rights reserved.</p>")
+document.writeln("    </br>")
---
+++ title.js
@@ -0,0 +1,1 @@
+document.writeln("   <title>DFLAG Group</title>")
